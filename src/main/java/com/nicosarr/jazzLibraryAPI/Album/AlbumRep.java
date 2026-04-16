36f84d88e369fae09d4a4560b1020fbee88a0a47
nickos sarr <nicko.sarr@gmail.com>
2026-04-16 11:49:04 +0300

    Bug 260416 Serve all kinds of albums... in the ui the album response were missing some album ids , at the same time the song table fk was pointing at those missing album ids, that created fk constreint error

--- src/main/java/com/nicosarr/jazzLibraryAPI/Album/AlbumRep.java
+++ src/main/java/com/nicosarr/jazzLibraryAPI/Album/AlbumRep.java
@@ -15,7 +15,7 @@ public class AlbumRep {
 
     // Retrieve all albums without artists
     public List<AlbumDTO> retrieveAll() {
-        String jpql = "SELECT a FROM Album a Where a.releaseType like 'album' ORDER BY a.album_id";
+        String jpql = "SELECT a FROM Album a ORDER BY a.album_id";
         TypedQuery<Album> query = entityManager.createQuery(jpql, Album.class);
         List<Album> albums = query.getResultList();
         return albums.stream().map(AlbumDTO::fromEntity).collect(Collectors.toList());
