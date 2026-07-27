286e5b81716a6273ed99e9b9ba2fd866a09f79de
nickos sarr <nicko.sarr@gmail.com>
2026-07-27 18:11:05 +0300

    Dev 270726 make the availability chek run in jobs

--- src/main/java/com/nicosarr/jazzLibraryAPI/JazzLibraryApiApplication.java
+++ src/main/java/com/nicosarr/jazzLibraryAPI/JazzLibraryApiApplication.java
@@ -3,6 +3,7 @@ package com.nicosarr.jazzLibraryAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // http://localhost:8080/swagger-ui/index.html
+// test : git working
 
 @SpringBootApplication
 public class JazzLibraryApiApplication {
